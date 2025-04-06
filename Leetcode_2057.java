420280b80b82e105aa82dfb7002eb8198f8ccf83
Suchandra <suchandra.paul1909@gmail.com>
2025-04-06 18:19:49 +0530

    Leetcode_2057_Smallest_Index_With_Equal_Value

---
+++ Leetcode_2057.java
@@ -0,0 +1,22 @@
+package DSA.Leetcode.Leetcode;
+
+public class Leetcode_2057 {
+    public static int smallestEqual(int[] nums) {
+        int minIndex = -1;
+        int index = 0, index2 = 0; 
+        for(int i = 0; i<nums.length; i++){
+            if(i % 10 == nums[i]){
+                System.out.println(i);
+                  return i;
+              }
+
+        }
+        System.out.println(-1);
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,3,2,1};
+        smallestEqual(arr);
+    }
+
+}
