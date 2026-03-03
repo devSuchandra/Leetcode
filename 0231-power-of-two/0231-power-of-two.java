86201a7fcce5a3e94c8df7611af15e639ad62a23
SUCHANDRA PAUL <suchandra.paul1909@gmail.com>
2026-03-04 01:48:26 +0530

    Time: 1 ms (96.13%), Space: 42.3 MB (97.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n % 2 != 0 && n != 1 || n == 0)
+        if(n % 2 != 0 && n != 1 || n <= 0)
         return false;
         if(n == 1){
         return true;
